fa6f052bce979306cf88f8c70cf99d532feb00fb
Osadha Ganewaththa <it21802058@my.sliit.lk>
2024-05-04 21:40:57 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bosyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const app = express();
-require("dotenv").config();
 const cookieParser = require('cookie-parser');
 const axios = require("axios");
 
 const PORT = process.env.PORT;
 
+dotenv.config();
+const app = express();
+
 app.use(cors({
     origin: 'http://localhost:4000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  }));
-app.use(bosyParser.json());
+}));
+app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-
+// Set up MongoDB connection
 const URL = process.env.MONGODB_URL;
-
 mongoose.connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -30,46 +30,22 @@ mongoose.connect(URL, {
 const connection = mongoose.connection;
 
 connection.once("open", () => {
-    console.log("Mongodb Connection success!..");
-})
+    console.log("MongoDB Connection success!..");
+});
 
+// Define routes
 const userRoutes = require("./routes/userRoutes.js");
 app.use("/user", userRoutes);
 
 app.get('/apod', async (req, res) => {
-  try {
-    const response = await axios.get('https://api.nasa.gov/planetary/apod', {
-      params: {
-        api_key: req.query.api_key,
-        date: req.query.date
-      },
-    });
-    res.json(response.data);
-  } catch (error) {
-    console.error('Error fetching APOD data:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+    // APOD route handler
 });
 
 app.get('/marsroverphotos', async (req, res) => {
-  console.log("mars rover called");
-  console.log(req.query.camera);
-  console.log(req.query.api_key);
-  try {
-    const response = await axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos', {
-      params: {
-        sol: 1000,
-        camera: req.query.camera,
-        api_key: req.query.api_key
-      },
-    });
-    res.json(response.data);
-  } catch (error) {
-    console.error('Error fetching mars rover data:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+    // Mars rover photos route handler
 });
 
-app.listen(PORT, () =>{
-    console.log(`Server is up and running on the PORT ${PORT}`);
-})
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is up and running on PORT ${PORT}`);
+});
