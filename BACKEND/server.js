6ded6eaba9a10b7d4c5f453f4ea88120d582cf31
Osadha Ganewaththa <it21802058@my.sliit.lk>
2024-05-05 16:54:22 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -16,7 +16,7 @@ const DEV_URL = process.env.DEV_URL;
 const MONGODB_URL = process.env.MONGODB_URL;
 
 app.use(cors({
-    origin: DEV_URL,
+    origin: "https://nasa-ap-is-hosting.vercel.app/",
     credentials: true
 }));
 
